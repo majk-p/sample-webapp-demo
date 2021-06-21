fb7463225fab39b8bf99148efa67ef4d686e2475
Michal Pawlik <admin@michalp.net>
2021-06-22 00:23:51 +0200

    implement http server

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,37 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import cats.effect._
+import sttp.tapir._
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+import org.http4s.HttpRoutes
+
+import cats.syntax.all._
+
+import scala.concurrent.ExecutionContext
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.Router
+import org.http4s.syntax.kleisli._
+
+object Hello extends IOApp {
+
+  val helloWorld: Endpoint[String, Unit, String, Any] =
+    endpoint.get.in("hello").in(query[String]("name")).out(stringBody)
+
+  def endpointLogic(name: String) = IO(s"Hello, $name!".asRight[Unit])
+  
+  val helloWorldRoutes: HttpRoutes[IO] = 
+    Http4sServerInterpreter.toRoutes(helloWorld)(endpointLogic _)
+
+  def serverResource(routes: HttpRoutes[IO])(ec: ExecutionContext) = 
+    BlazeServerBuilder[IO](ec)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(Router("/" -> routes).orNotFound)
+      .resource 
+
+  override def run(args: List[String]): IO[ExitCode] = 
+    serverResource(helloWorldRoutes)(executionContext)
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
+  
 }
